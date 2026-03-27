1ff0d8f13e3b4efd82250956d60b45ceea5d92c4
Ma666-s <salehmomo669@gmail.com>
2026-03-27 14:56:51 +0200

    Fix routing: redirect root path to /dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,17 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '**', redirectTo: '' }
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'     // مهم جدًا
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
 ];
